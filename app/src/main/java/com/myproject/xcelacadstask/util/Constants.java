6dbe827bd2b3bd160d64f92460e9013c69be1934
Rahul Raghuwanshi <52368582+thisrahul@users.noreply.github.com>
2021-09-08 21:22:03 +0530

    Update Constants.java

--- app/src/main/java/com/myproject/xcelacadstask/util/Constants.java
+++ app/src/main/java/com/myproject/xcelacadstask/util/Constants.java
@@ -3,6 +3,6 @@ package com.myproject.xcelacadstask.util;
 public class Constants {
 
     public static final String BASE_URL = "https://www.googleapis.com/youtube/v3/";
-    public static final String KEY = "AIzaSyDcFYPzyxVPlOE0VEzKKm1VHnZOAiLhAPE";
+    public static final String KEY = "GOOGLE_API_KEY";
     public static final String max = "10";
 }
